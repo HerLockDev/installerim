dddff2c2bb0f6afe08ba5544a0eb959fd462cf73
HerLockDev <94521956+HerLockDev@users.noreply.github.com>
2021-11-21 13:37:13 +0400

    Update astring.py

--- nitro_installer/astring.py
+++ nitro_installer/astring.py
@@ -6,7 +6,7 @@ import asyncio
 import os
 import sys
 import subprocess
-from neon_installer import hata, bilgi, onemli, soru
+from nitro_installer import hata, bilgi, onemli, soru
 
 from telethon import TelegramClient, events, version
 from telethon.errors import SessionPasswordNeededError, PhoneCodeInvalidError, PasswordHashInvalidError, PhoneNumberInvalidError
